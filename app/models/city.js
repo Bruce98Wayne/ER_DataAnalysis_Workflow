56565512c0e00ceb31c4c0d80e9522f50e82c7c6
LordVoldemort28 <rahul.prajapati90904@gmail.com>
2020-04-17 22:04:22 -0500

    Added city and state

---
+++ app/models/city.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('City', {
+    cityId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull : true
+    },
+}, {});
+  City.associate = function(models) {
+    // associations can be defined here
+    models.City.hasMany(models.Users, {foreignKey: 'userId'})
+  };
+  return City;
+};
---
+++ app/models/state.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('State', {
+    stateId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull : true
+    },
+}, {});
+  State.associate = function(models) {
+    // associations can be defined here
+    models.State.hasMany(models.City, {foreignKey: 'cityId'})
+  };
+  return State;
+};
